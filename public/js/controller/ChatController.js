d2f5cf2dd68c199a070c1c3d6df6535be6eb9db4
Çağatay Sarp Aksu <sarpcaksu@gmail.com>
2018-02-23 10:17:58 +0300

    message logs are at bottom

--- public/js/controller/ChatController.js
+++ public/js/controller/ChatController.js
@@ -3,7 +3,6 @@ ChatController.AddChatLog = function chatcontroller_display(parent, data) {
     // console.log("chatcontroller_display1");
     
     var log;
-    debugger;
     if (ConnectionController.connectionUser === data.sender) {
         log = Object.create(UserChatLogView);
     } else {
